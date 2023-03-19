7e7e43229cb4164042b1fe7b7ba4f654a676bb02
1c2t1z <95414249+1c2t1z@users.noreply.github.com>
2023-03-19 22:33:43 +0100

    amelioration + creation
    footer fait, normalement j'ai juste à poursuivre. faut juste faire attention à l'encodage. pour accent dans footer. voir si rubrique suplémentaire pour aside ou si je met deux choix pour info et erreur.

--- script aide.bat
+++ script aide.bat
@@ -1,204 +1,124 @@
-@echo off
-setlocal EnableDelayedExpansion
 ::=== PREPARATION ===
-@title cerber.re - generateur code HTML - 1c2t1z
-@color 0f
+@echo off
+@title Script aide ~ Gnrateur code HTML ~ 1c2t1z
+@setlocal EnableExtensions EnableDelayedExpansion
+color 1f
+::=== VARIABLE ===
+REM ESPACE INSECABLE POUR DEBUT LINE 2
+set "notepad.caution.message.line=2"
+set "notepad.caution.message.line1=- Lorsque vous avez termin, fermer le bloc note puis"
+set "notepad.caution.message.line2=ÿÿrevenez sur cette page..."
 ::=== DEMARRAGE ===
-:debut
-color 0f
-cls
+:start
+::=== NETTOYAGE ===
+if exist %temp%\html-*.txt (del /q %temp%\html-*.txt)
 ::=== MENU PRINCIPAL ===
+cls
+echo ษออออออออออออออออออออออออออออออออออออออออออออออออออออออป
+echo บ                     MENU PRINCIPAL                   บ
+echo ศออออออออออออออออออออออออออออออออออออออออออออออออออออออผ
+echo ษออออออออออออออออออออออออออออออออออออออออออออออออออออออป
+echo บ [A] - HTML                                           บ
+echo บ [B] - CSS                                            บ
+echo บ                                                      บ
+echo บ [X] - QUITTER                                        บ
+echo ศออออออออออออออออออออออออออออออออออออออออออออออออออออออผ
 echo.
-set choixdisp=123
-set amount=3
-echo. [1] - HTML
-echo. [2] - CSS (pas creer)
-echo.
-echo. [3] - quitter
-echo.
-choice /c !choixdisp:~0,%amount%! /N /M "- Que veux tu copier ?"  
-goto :%errorlevel% 
+choice /C:ABX /N /M "- [A/B/X] Quel est votre choix ? "
+if errorlevel 3  goto :exit
+if errorlevel 2  goto :css
+if errorlevel 1  goto :html.v
 ::=== HTML CONFIRMATION ===
-:1
+:html.v
 cls
+echo ษออออออออออออออออออออออออออออออออออออออออออออออออออออออป
+echo บ                    HTML VALIDATION                   บ
+echo ศออออออออออออออออออออออออออออออออออออออออออออออออออออออผ
+echo ษออออออออออออออออออออออออออออออออออออออออออออออออออออออป
+echo บ [O] - OUI                                            บ
+echo บ [N] - NON                                            บ
+echo ศออออออออออออออออออออออออออออออออออออออออออออออออออออออผ
 echo.
-set choixdisp=12
-set amount=2
-echo. [1] - OUI
-echo. [2] - NON
-echo.
-choice /c !choixdisp:~0,%amount%! /N /M "- Tu as choisi HTML ?"  
-goto :HTML_V%errorlevel%
-::=== HTML VALIDATION ===
-REM OUI
-:HTML_V1
-goto :HTML_MAIN_MENU
-REM NON
-:HTML_V2
-goto :debut
+choice /C:ON /N /M "- [O/N] Etes-vous sur de votre choix ? "
+if errorlevel 2  goto :html.no
+if errorlevel 1  goto :html.yes
+:html.yes
+goto :html
+:html.no
+goto :start
 :::=== HTML MENU PRINCIPAL ===
-:HTML_MAIN_MENU
+:html
+cls
+echo ษออออออออออออออออออออออออออออออออออออออออออออออออออออออป
+echo บ                    HTML COPIE CODE                   บ
+echo ศออออออออออออออออออออออออออออออออออออออออออออออออออออออผ
+echo ษออออออออออออออออออออออออออออออออออออออออออออออออออออออป
+echo บ NB : Il est a noter que certains code sont gnrique บ
+echo บ     donc a adapter en fonction de la situation.      บ
+echo บ                                                      บ
+echo บ INFO :   Au niveau du fonctionnement, vous faites    บ
+echo บ          votre choix parmis ceux propos juste au    บ
+echo บ          dessous puis un bloc note avec le code      บ
+echo บ          s'ouvre, vous copiez et fermez la fenetre.  บ
+echo ศออออออออออออออออออออออออออออออออออออออออออออออออออออออผ
+echo ษออออออออออออออออออออออออออออออออออออออออออออออออออออออป
+echo บ [A] - HEAD (meta, :og, favicon...)                   บ
+echo บ [B] - HEADER (nav id...)                             บ
+echo บ [C] - MAIN (main id, content...)                     บ
+echo บ [D] - PRE CODE (cmd, html, css, ps...)               บ
+echo บ [E] - PRE CODE (console like cmd, ps)                บ
+echo บ [F] - ASIDE (bulle info et erreur)                   บ
+echo บ [G] - FOOTER (copyfuck, mask-icon...)                บ
+echo บ                                                      บ
+echo บ [X] - AUCUN (revenir a l'accueil)                    บ
+echo ศออออออออออออออออออออออออออออออออออออออออออออออออออออออผ
+echo.
+choice /C:ABCDEFGX /N /M "- [A/B/C/D/E/F/G/X] Quel code voulez-vous copier ? "
+if errorlevel 8 goto :start
+if errorlevel 7 goto :html.footer
+if errorlevel 6 goto :html.aside
+if errorlevel 5 goto :html.precode.console
+if errorlevel 4 goto :html.precode.language
+if errorlevel 3 goto :html.main
+if errorlevel 2 goto :html.header
+if errorlevel 1 goto :html.head
+
+:html.footer
 cls
 echo.
-echo HTML code a copier
-echo il est a noter que tout les codes sont generique
-echo donc il faut les remplir en fonction du besoin.
-echo.
-set choixdisp=123456789
-set amount=9
-echo. [1] - head (1er)
-echo. [2] - header (nav id etc...)
-echo. [3] - div content (contenu)
-echo. [4] - code (chgmt fonction language)
-echo. [5] - code (console cmd ou PS)
-REM 6 & 7 sont quasi pareil voir comment je peux les reunir
-echo. [6] - aside (info et erreur)
-echo. [7] - footer
-echo.
-echo. [8] - suivant
-echo.
-echo. [9] - retour
-echo.
-choice /c !choixdisp:~0,%amount%! /N /M "- Que veux tu copier ?"  
-goto :HTMLc%errorlevel%
-
-::=== HTML choix 9 BACK ===
-:HTMLc9
-cls
-color a
-echo.
-echo. retour a l'accueil !
-ping -n 2 127.0.0.1>nul
-goto :debut
-
-:HTMLc8
-goto :HTML_SECONDARY_MENU
-
-:HTMLc7
-cls
-echo.
-echo un fichier va s'ouvrir il contient les donnees pour creer un footer.
-echo une fois la copie faites revenez sur ce script.
-echo.
-echo appuyez pour commencer
+echo - Appuyez sur une touche pour commencer...
 pause > nul
 for /f "useback delims=" %%_ in (%0) do (
-  if "%%_"=="___ATAD1___" set $=
-if defined $ echo(%%_ >> %temp%\test.txt
-  if "%%_"=="___DATA1___" set $=1
+  if "%%_"=="___RETOOF___" set $=
+if defined $ echo(%%_ >> %temp%\html-footer.txt
+  if "%%_"=="___FOOTER___" set $=1
 )
-goto :call_footer_html
-___DATA1___
-<footer><a href="/"><img src="mask-icon.svg" width="512" height="512" alt="">Cerber</a><ul id="copyfuck"><li>Aucun droit rรฉservรฉ - Fait par CT</li></ul></footer>
-___ATAD1___
-:call_footer_html
-call %temp%\test.txt
-pause
-del /q %temp%\test.txt
+goto :call.html.footer
+___FOOTER___
+<footer><a href="/"><img src="/mask-icon.svg" width="512" height="512" alt="">Cerber</a><ul id="copyfuck"><li>Aucun droit rserv - Fait par CT</li></ul></footer>
+___RETOOF___
+:call.html.footer
 cls
 echo.
-echo retour automatique vers le MENU HTML PRINCIPAL
+for /l %%A in (1,1,%notepad.caution.message.line%) do (for /f "tokens=*" %%B in ("!notepad.caution.message.line%%A!") do (echo %%B))
+call %temp%\html-footer.txt
+del /q %temp%\html-footer.txt
+cls
+echo.
+echo - Retour automatique vers le menu : "HTML COPIE CODE"...
 ping -n 4 127.0.0.1>nul
-goto :HTML_MAIN_MENU
-
-
-:HTMLc6
-
-:::=== HTML MENU SECONDAIRE ===
-:HTML_SECONDARY_MENU
+goto :html
+::=== MENU PRINCIPAL -> CSS ===
+:css
 cls
 echo.
-echo HTML code a copier
-echo il est a noter que tout les codes sont generique
-echo donc il faut les remplir en fonction du besoin.
-echo.
-set choixdisp=12345678
-set amount=8
-echo. [1] - a definir
-echo. [2] - a definir
-echo. [3] - a definir
-echo. [4] - a definir
-echo. [5] - a definir
-echo. [6] - a definir
-echo. [7] - a definir
-echo.
-echo. [8] - retour
-echo.
-choice /c !choixdisp:~0,%amount%! /N /M "- Que veux tu copier ?"  
-goto :HTMLcc%errorlevel%
-
-:HTMLcc8
-goto :HTML_MAIN_MENU
-
-REM DEB CI 
-
-set MAX_LIGNES=3
-set ligne1=%SYSTEMDRIVE%\Users\%matricule%\AppData\Roaming\Zoom\uninstall\Installer.exe /uninstall /S
-set ligne2=%SYSTEMDRIVE%\Users\%matricule%\AppData\Zoom\uninstall\Installer.exe /uninstall /S
-set ligne3=del %APPDATA%\Microsoft\Windows\Start Menu\Programs\Startup\Zoom.Ink
-
-REM BOUCLE ECRITURE VERS ok.bat
-
-for /l %%A in (1,1,%MAX_LIGNES%) do (
-    for /f "tokens=*" %%B in ("!ligne%%A!") do (
-        echo %%B >> ok.bat
-        )
-    )
-
-REM ENVOIE DE ok.bat VERS LE PC + AUTOSUPRESSION
-
-powershell.exe -command xcopy $env:USERPROFILE\Desktop\ok.bat '\\%nom%\c$\ProgramData\Microsoft\Windows\Start Menu\Programs\Startup'
-Del /Q %USERPROFILE%\desktop\ok.bat
-Del /Q ok.bat
+echo - Rubrique pas encore crer, redmarrage du script en cours...
+ping -n 5 127.0.0.1>nul
+goto :start
+::=== MENU PRINCIPAL -> QUITTER ===
+:exit
 cls
-
-REM FIN CI
-
-goto log
-
-
-REM SUPRESSION RESIDUS
-
-:: CREATION FICHIER LOG SUR BUREAU
-:log
-set datetimef=%date:~0,2%:%date:~3,2%:%date:~-4%-%time:~0,2%:%time:~3,2%:%time:~6,2%
-cd /D "%userprofile%\desktop"
-echo %datetimef% - matricule: %matricule% - nom de poste: %nom% >> log_dezoomage.txt
-ping -n 1 127.0.0.1>nul
-cls
-echo Ping en cours 
-echo Veuillez patientez 3 sec...
-ping -a %nom% >> log_dezoomage.txt
+echo.
+echo - Nettoyage et fermeture du script en cours...
 ping -n 3 127.0.0.1>nul
-cls
-
-REM OUVERTURE LOG
-
-echo Ouverture du LOG...
-ping -n 2 127.0.0.1>nul
-cls
-start log_dezoomage.txt
-
-REM SCRIPT FERMETURE NOTEPAD
-
-set MAX_LIGNES=4
-set ligne1=test
-set ligne2=test
-set ligne3=test
-set ligne4=test
-
-for /l %%A in (1,1,%MAX_LIGNES%) do (
-    for /f "tokens=*" %%B in ("!ligne%%A!") do (
-        echo %%B >> %temp%\test.txt
-        )
-    )
-start test.txt
-::=== CHOIX 2 CSS ===
-:2
-echo.
-echo rubrique pas encore creer, redemarrage du script en cours... !
-ping -n 4 127.0.0.1>nul
-goto :debut
-::=== CHOIX 3 EXIT ===
-:3
-exit
+goto :eof
