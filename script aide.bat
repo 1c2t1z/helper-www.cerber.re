71fa6d5968c3dafc7d4f4c7d91bf4105a666a69e
1c2t1z <95414249+1c2t1z@users.noreply.github.com>
2023-03-26 13:08:44 +0200

    creation + amelioration

--- script aide.bat
+++ script aide.bat
@@ -1,7 +1,7 @@
-REM ### POUR LE BUG DES ACCENTS POUR FOOTER ET "A COMPLETER"
-REM ### IL FAUT CONVERTIR EN ANSI PUIS COMPLETER LES , PAR DES
-REM ### ACCENTS. FAIRE SAVE DU CODE AVANT. C'EST A FAIRE A LA
-REM ### TOUTE FIN.
+:: ### POUR LE BUG DES ACCENTS POUR FOOTER ET "A COMPLETER"
+:: ### IL FAUT CONVERTIR EN ANSI PUIS COMPLETER LES , PAR DES
+:: ### ACCENTS. FAIRE SAVE DU CODE AVANT. C'EST A FAIRE A LA
+:: ### TOUTE FIN.
 
 ::=== PREPARATION ===
 @echo off
@@ -93,6 +93,7 @@ if errorlevel 4 goto :html.precode.language
 if errorlevel 3 goto :html.main
 if errorlevel 2 goto :html.header
 if errorlevel 1 goto :html.head
+:: 7 OK
 :html.footer
 cls
 echo ษออออออออออออออออออออออออออออออออออออออออออออออออออออออป
@@ -113,7 +114,11 @@ if defined $ echo(%%_ >> %temp%\html-footer.txt
 )
 goto :call.html.footer
 ___FOOTER___
-<footer><a href="/"><img src="/mask-icon.svg" width="512" height="512" alt="">Cerber</a><ul id="copyfuck"><li>Aucun droit rserv - Fait par CT</li></ul></footer>
+<footer><a href=/><img alt="" src=/mask-icon.svg height=512 width=512>Cerber</a>
+    <ul id=copyfuck>
+        <li>Aucun droit rserv - Fait par CT
+    </ul>
+</footer>
 ___RETOOF___
 :call.html.footer
 cls
@@ -121,6 +126,78 @@ echo.
 for /l %%A in (1,1,%notepad.caution.message.line%) do (for /f "tokens=*" %%B in ("!notepad.caution.message.line%%A!") do (echo %%B))
 call %temp%\html-footer.txt
 del /q %temp%\html-footer.txt
+cls
+echo.
+echo - Retour automatique vers le menu : "HTML COPIE CODE"...
+ping -n 4 127.0.0.1>nul
+goto :html
+:: 1 EN COURS
+:: ERROR PAGE FAIT, CLASSIC A FAIRE
+:html.head
+cls
+echo ษออออออออออออออออออออออออออออออออออออออออออออออออออออออป
+echo บ                  HEAD TYPE DE PAGE                   บ
+echo ศออออออออออออออออออออออออออออออออออออออออออออออออออออออผ
+echo ษออออออออออออออออออออออออออออออออออออออออออออออออออออออป
+echo บ [A] - PAGE D'ERREUR                                  บ
+echo บ [B] - PAGE CLASSIQUE                                 บ
+echo บ                                                      บ
+echo บ [X] - RETOUR                                         บ
+echo ศออออออออออออออออออออออออออออออออออออออออออออออออออออออผ
+echo.
+choice /C:ABX /N /M "- [A/B/X] Etes-vous sur de votre choix ? "
+if errorlevel 3 goto :html
+if errorlevel 2 goto :html.head.classic.page
+if errorlevel 1 goto :html.head.error.page
+:html.head.error.page
+cls
+for /l %%A in (1,1,%all.instruction.message.line%) do (for /f "tokens=*" %%B in ("!all.instruction.message.line%%A!") do (echo %%B))
+echo ษออออออออออออออออออออออออออออออออออออออออออออออออออออออป
+echo บ INFO : Pas besoin de mettre les balises head en      บ
+echo บ avec HTML 5 elles sont immdiatement reconnu. Par    บ
+echo บ soucis d'optimisation je ne fournis que la version   บ
+echo บ minify.                                              บ
+echo ศออออออออออออออออออออออออออออออออออออออออออออออออออออออผ
+echo.
+echo - Veuillez appuyer sur une touche pour lancer la copie...
+pause > nul
+for /f "useback delims=" %%_ in (%0) do (
+  if "%%_"=="___RORRE-DAEH-LMTH___" set $=
+if defined $ echo(%%_ >> %temp%\html-head-error-page.txt
+  if "%%_"=="___HTML-HEAD-ERROR___" set $=1
+)
+goto :call.html.head.error.page
+___HTML-HEAD-ERROR___
+<html lang=fr prefix="og: https://ogp.me/ns#">
+    <meta charset=utf-8>
+    <title>Erreur 401 | Cerber</title>
+    <meta content=Unauthorized name=description>
+    <meta content="width=device-width,initial-scale=1" name=viewport>
+    <meta content="dark light" name=color-scheme>
+    <meta content=#fcfcff name=theme-color>
+    <meta content=#ffffff name=msapplication-TileColor>
+    <meta content=/mstile/144x144.png name=msapplication-TileImage>
+    <meta content="Erreur 401 | Cerber" property=og:title>
+    <meta content=Unauthorized property=og:description>
+    <meta content=website property=og:type>
+    <meta content=https://www.cerber.re/icon/opengraph.png property=og:image>
+    <meta content=512 property=og:image:width>
+    <meta content=512 property=og:image:height>
+    <meta content="Cerber Logo" property=og:image:alt>
+    <meta content=Cerber property=og:site_name>
+    <link href=/6ad46ada.main.css rel=stylesheet>
+    <link href=/mask-icon.svg rel=mask-icon color=#1a1c1e>
+    <link href=/favicon.svg rel=icon type=image/svg+xml>
+    <link href=/apple-touch-icon.png rel=apple-touch-icon>
+    <link href=/manifest.json rel=manifest>
+    <link href=/licence.txt rel=license>
+___RORRE-DAEH-LMTH___
+:call.html.head.error.page
+cls
+echo.
+for /l %%A in (1,1,%notepad.caution.message.line%) do (for /f "tokens=*" %%B in ("!notepad.caution.message.line%%A!") do (echo %%B))
+call %temp%\html-head-error-page.txt
+del /q %temp%\html-head-error-page.txt
 cls
 echo.
 echo - Retour automatique vers le menu : "HTML COPIE CODE"...
@@ -138,5 +215,6 @@ goto :start
 cls
 echo.
 echo - Nettoyage et fermeture du script en cours...
+if exist %temp%\html-*.txt (del /q %temp%\html-*.txt)
 ping -n 3 127.0.0.1>nul
 goto :eof
